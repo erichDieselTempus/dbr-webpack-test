5b50b6af8b7c896ad6f44c8d3bfb56a49783000a
erichDieselTempus <86798926+erichDieselTempus@users.noreply.github.com>
2023-03-28 20:27:32 -0500

    Overlay fleshed out
    Using grid styling to draw onto the camera overlay.  Hope to use the same parameters to locate barcodes in the grid when parsing the table.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-// Online demo did not build properly, it was only importing the Barcode Scanner Object.
-// Resolved by importing entire module and prepending dbr to all BarcodeScanner references.
+// Online demo did not build properly, needed to add curly brace around the import
+/* Useful Links
+ -- https://www.dynamsoft.com/barcode-reader/docs/web/programming/javascript/samples-demos/helloworld-webpack.html
+ -- https://www.dynamsoft.com/barcode-reader/docs/web/programming/javascript/user-guide/
 
-import * as dbr from "dynamsoft-javascript-barcode";
-dbr.BarcodeScanner.license = 'DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAxNzU1MzczLVRYbFhaV0pRY205cVgyUmljZyIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21sdHMuZHluYW1zb2Z0LmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAxNzU1MzczIiwiY2hlY2tDb2RlIjoyODA2NjU1NTV9';
-dbr.BarcodeScanner.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode/dist/";
+
+document.getElementById('UIElement').appendChild(scanner.getUIElement());
+
+*/
+
+
+import {BarcodeScanner} from "dynamsoft-javascript-barcode";
+BarcodeScanner.license = 'DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAxNzU1MzczLVRYbFhaV0pRY205cVgyUmljZyIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21sdHMuZHluYW1zb2Z0LmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAxNzU1MzczIiwiY2hlY2tDb2RlIjoyODA2NjU1NTV9';
+BarcodeScanner.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode/dist/";
 
 let pScanner = null;
+
+
 if (document.getElementById('readBarcode')) {
     document.getElementById('readBarcode').onclick = async function() {
         try {
-            let scanner = await (pScanner = pScanner || dbr.BarcodeScanner.createInstance());
+            //BarcodeScanner.defaultUIElementURL = "./src/dbr.ui.html";
+            let scanner = await (pScanner = pScanner || BarcodeScanner.createInstance());
+            await scanner.setUIElement(document.getElementById('div-ui-container'));
             scanner.onFrameRead = results => {
                 console.log("Barcodes on one frame:");
                 for (let result of results) {
@@ -22,6 +34,7 @@ if (document.getElementById('readBarcode')) {
                 console.log("Unique Code Found: " + result);
             }
             await scanner.show();
+            
         } catch (ex) {
             alert(ex.message);
             throw ex;
